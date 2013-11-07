c7ad9efd57e963f42d4dddfc9d3ddbc5f6ac9084
Serhiy Malyy <serhiy.malyy@gmail.com>
2013-11-06 22:06:15 -0800

    hw21 100% done

---
+++ hw_21/run_script_21_04.bat
@@ -0,0 +1,6 @@
+:: Commands to execute script script_21.x.rb
+echo Long Options:
+ruby ./lib/script_21_04.rb --input %HOMEDRIVE%%HOMEPATH%/ita/hw_21/etc/input_21_04.txt
+echo Short Option
+ruby ./lib/script_21_04.rb -i %HOMEDRIVE%%HOMEPATH%/ita/hw_21/etc/input_21_04.txt
+pause
---
+++ hw_21/run_script_21_06_A.bat
@@ -0,0 +1,6 @@
+:: Commands to execute script script_21.x.rb
+echo Long Options:
+ruby ./lib/script_21_06_A.rb --input %HOMEDRIVE%%HOMEPATH%/ita/hw_21/etc/input_21_06_A.txt
+echo Short Option
+ruby ./lib/script_21_06_A.rb -i %HOMEDRIVE%%HOMEPATH%/ita/hw_21/etc/input_21_06_A.txt
+pause
