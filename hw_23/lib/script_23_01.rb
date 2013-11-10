2a6a6802d09b011f60045db32ea0089d0903e24c
Serhiy Malyy <serhiy.malyy@gmail.com>
2013-11-09 17:37:05 -0800

    23.1 finally works

--- hw_23/lib/script_23_01.rb
+++ hw_23/lib/script_23_01.rb
@@ -1,28 +1,29 @@
  # ========================================================================
- # Script     =  script_21_01.rb
+ # Script     =  script_23_01.rb
  # ========================================================================
- # Description   =  "The script in accepting path argument , reads through file 
+ # Description   =  "The script in accepting path argument , reads through JSON file 
  #                   and returns line data.
  # Name          =  "Serhiy Malyy"
  # Email         =  "ThisIsPublicGit@nobody.com"
  # ========================================================================
 require 'optparse'
-require 'jason'
-
+require 'json'
+json_file = ""
  # OptionParser is a class for command-line option analysis
-OptionParser.new do |opts| # opts is var 
+ OptionParser.new do |opts| # opts is var 
   # Option one 
 	opts.on("-i", "--input") do
 		$file_name = ARGV[0]  # <<- GLOBAL ($) VAR a
-		# puts ARGV[0]
+		 #puts ARGV[0]
 	end 
-end.parse!
+ end.parse!
  
 	json_file = File.read($file_name)
-	puts json_file.class "Class Of JSON File.Read"
-	element = JASON.parse(json_file)
-	puts element.class
+	puts json_file #"Class Of JSON File.Read"
+	element = JSON.parse(json_file)
+	#puts element[fruit_a]
+	#puts element.class
 	
 
  #return the output to command line . Note.chomp method = remove new line spaces 
- puts "My favorite fruits are: #{element[fruit_a]}s and #{element[fruit_b]}s"
+ puts "My favorite fruits are: #{element["fruit_a"]}s and #{element["fruit_b"]}s"
